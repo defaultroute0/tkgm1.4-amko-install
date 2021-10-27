73db96beb9edab0b6191012b8f7cb1e94648ba67
root <root@localhost.localdomain>
2021-10-28 05:33:51 +1100

    update

--- install-ako-tkgm-guest2.sh
+++ install-ako-tkgm-guest2.sh
@@ -1,2 +1,2 @@
 kubectl create ns avi-system
-helm install  ako/ako  --generate-name --version 1.5.1 -f ./values-tkgm-guest2.yaml --namespace=avi-system
+helm install  ako/ako  --generate-name --version 1.5.2 -f ./values-tkgm-guest2.yaml --namespace=avi-system
