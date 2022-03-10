7eebc88ddcb14f30f7c0e14c175ff76814aa911c
Ryan Douglas <67136481+defaultroute0@users.noreply.github.com>
2022-03-10 11:42:27 +1100

    Update install-amko.sh

--- amko/install-amko.sh
+++ amko/install-amko.sh
@@ -3,3 +3,4 @@
 #cp ~/.kube/config gslb-members
 kubectl create secret generic gslb-config-secret --from-file gslb-members -n avi-system
 helm install  ako/amko  --generate-name --version 1.5.2 -f values.yaml --namespace=avi-system
+# do above in each cluster
