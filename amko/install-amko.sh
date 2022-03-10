4dfa3f8f0ca417593f675d32ee66fefa6a07130b
Ryan Douglas <67136481+defaultroute0@users.noreply.github.com>
2022-03-10 11:45:13 +1100

    Update install-amko.sh

--- amko/install-amko.sh
+++ amko/install-amko.sh
@@ -4,3 +4,11 @@
 kubectl create secret generic gslb-config-secret --from-file gslb-members -n avi-system
 helm install  ako/amko  --generate-name --version 1.5.2 -f values.yaml --namespace=avi-system
 # do above in each cluster
+
+
+
+#NOTES:
+#1. Get the application URL by running these commands:
+#  export POD_NAME=$(kubectl get pods --namespace avi-system -l "app.kubernetes.io/name=amko,app.kubernetes.io/instance=amko-1646873058" -o jsonpath="{.items[0].metadata.name}")
+#  echo "Visit http://127.0.0.1:8080 to use your application"
+#  kubectl --namespace avi-system port-forward $POD_NAME 8080:80
